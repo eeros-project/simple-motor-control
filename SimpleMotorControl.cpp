2a657e089546b3a696cb633e7fdbaa8366eb5b69
ursgraf <urs.graf@ntb.ch>
2020-06-10 14:25:38 +0200

    Register signal handler after creation of CS
    This is used for librobotcontrol on the bbb.

--- SimpleMotorControl.cpp
+++ SimpleMotorControl.cpp
@@ -24,44 +24,43 @@ using namespace eeros::sequencer;
 const double dt = 0.001;
 
 void signalHandler(int signum){
-	SafetySystem::exitHandler();
-	Sequencer::instance().abort();
+  SafetySystem::exitHandler();
+  Sequencer::instance().abort();
 }
 
 int main(int argc, char **argv) {
-	signal(SIGINT, signalHandler);
-	
-	StreamLogWriter w(std::cout);
-	Logger::setDefaultWriter(&w);
-	Logger log;
-	w.show();
-	
-	log.info() << "Simple Motor Controller Demo started...";
-	
-	log.info() << "Initializing Hardware...";
-	HAL& hal = HAL::instance();
-	hal.readConfigFromFile(&argc, argv);
-	
-	// Create the control system
-	ControlSystem controlSys(dt);
-	
-	// Create and initialize a safety system
-	SMCSafetyProperties properties(controlSys, dt);
-	SafetySystem safetySys(properties, dt);
-	controlSys.timedomain.registerSafetyEvent(safetySys, properties.doEmergency);
-	
-	auto& sequencer = Sequencer::instance();
-	MainSequence mainSequence("Main Sequence", sequencer, safetySys, properties, controlSys, 3.14/10);
-	sequencer.addSequence(mainSequence);
-	mainSequence.start();
-	
-	auto &executor = Executor::instance();
-	executor.setMainTask(safetySys);
-	safetySys.triggerEvent(properties.doSystemOn);
-	
-	executor.run();
-	
-	mainSequence.waitAndTerminate();
-	log.info() << "Example finished...";
-	return 0;
+  StreamLogWriter w(std::cout);
+  Logger::setDefaultWriter(&w);
+  Logger log;
+  w.show();
+  
+  log.info() << "Simple Motor Controller Demo started...";
+  
+  log.info() << "Initializing Hardware...";
+  HAL& hal = HAL::instance();
+  hal.readConfigFromFile(&argc, argv);
+  
+  // Create the control system
+  ControlSystem controlSys(dt);
+  
+  // Create and initialize a safety system
+  SMCSafetyProperties properties(controlSys, dt);
+  SafetySystem safetySys(properties, dt);
+  controlSys.timedomain.registerSafetyEvent(safetySys, properties.doEmergency);
+  signal(SIGINT, signalHandler);
+
+  auto& sequencer = Sequencer::instance();
+  MainSequence mainSequence("Main Sequence", sequencer, safetySys, properties, controlSys, 3.14/10);
+  sequencer.addSequence(mainSequence);
+  mainSequence.start();
+  
+  auto &executor = Executor::instance();
+  executor.setMainTask(safetySys);
+  safetySys.triggerEvent(properties.doSystemOn);
+  
+  executor.run();
+  
+  mainSequence.waitAndTerminate();
+  log.info() << "Example finished...";
+  return 0;
 }
